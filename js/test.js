d9232c7f26d402e4a1cf67ccfe75668a12029800
imtangry <tang.rui,yu@foxmial.com>
2018-03-27 20:15:20 +0800

    顶部栏99% 搜索栏20%

--- js/test.js
+++ js/test.js
@@ -17,12 +17,15 @@ console.log(student.weight);
 function Cat(name) {
     //
     this.name=name;
-    this.say=()=>"Hello, "+this.name+"!";
+    // this.say=()=>"Hello, "+this.name+"!";
 }
+Cat.prototype.say=()=>"Hello, "+this.name+"!";
 
 var kitty = new Cat('Kitty');
 var heo=new Cat('Heo');
-kitty.say();
+console.log(kitty.say());
+console.log(heo.say());
+
 console.log( kitty.say === heo.say);
 
 var a=2;
@@ -30,4 +33,12 @@ var a=2;
     // 如果a是参数传入，我们可以把它当做是局部变量
 a=3;
 })();
+let bbb;
 console.log(a)
+console.log(bbb);
+
+
+bn="12"
+cn=bn-0;
+console.log(typeof(cn));
+// 原型链上加的方法，才会是===的？？？
